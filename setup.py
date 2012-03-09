b6bd1747ea063b8ba1e3b16dbf7cd953cd8710b4
Trent Nelson <trent@trent.me>
2012-03-09 12:14:34 -0500

    Hack setup.py a bit more (still doesn't work).

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-
+import os
 import sys
+
 vi = sys.version_info
 if vi.major != 2 or vi.minor not in (6, 7):
     raise Exception("Enversion requires either Python 2.6 or 2.7.")
@@ -12,17 +13,32 @@ except ImportError:
     has_setuptools = False
     from distutils.core import setup
 
-setup(
-    name='enversion',
-    version='0.1',
-    license=open('LICENSE').read(),
-    description='Enterprise Subversion Framework',
-    author='Trent Nelson',
-    author_email='trent@snakebite.org',
-    url='http://www.enversion.org/',
-    py_modules=['evn'],
-    scripts=['scripts/evnadmin'],
-)
+def find_packages(dir_):
+    packages = []
+    for pkg in ['evn']:
+        for _dir, subdirectories, files in (
+                os.walk(os.path.join(dir_, pkg))
+            ):
+            if '__init__.py' in files:
+                lib, fragment = _dir.split(os.sep, 1)
+                packages.append(fragment.replace(os.sep, '.'))
+    return packages
 
+def run_setup():
+    setup(
+        name='enversion',
+        version='0.1',
+        license=open('LICENSE').read(),
+        description='Enterprise Subversion Framework',
+        author='Trent Nelson',
+        author_email='trent@snakebite.org',
+        url='http://www.enversion.org/',
+        packages=find_packages(''),
+        package_dir={'evn': 'evn'},
+        scripts=['scripts/evnadmin'],
+    )
+
+if __name__ == '__main__':
+    run_setup()
 
 # vim:set ts=8 sw=4 sts=4 tw=78 et:
