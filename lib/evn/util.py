074d932b05fecfbaee0d654f76da390dbe916302
Trent Nelson <trent@trent.me>
2014-03-24 22:18:57 -0400

    Import @memoize/@memoized decorators (from conda.utils).

--- lib/evn/util.py
+++ lib/evn/util.py
@@ -20,6 +20,11 @@ from pprint import (
 
 from functools import (
     wraps,
+    partial,
+)
+
+from collections import (
+    Hashable,
 )
 
 from subprocess import (
@@ -365,6 +370,49 @@ def pid_exists(pid):
         else:
             return True
 
+#=============================================================================
+# Memoize Helpers (lovingly stolen from conda.utils)
+#=============================================================================
+class memoized(object):
+    """Decorator. Caches a function's return value each time it is called.
+    If called later with the same arguments, the cached value is returned
+    (not reevaluated).
+    """
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+    def __call__(self, *args):
+        if not isinstance(args, Hashable):
+            # uncacheable. a list, for instance.
+            # better to not cache than blow up.
+            return self.func(*args)
+        if args in self.cache:
+            return self.cache[args]
+        else:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+
+# For instance methods only
+class memoize(object): # 577452
+    def __init__(self, func):
+        self.func = func
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self.func
+        return partial(self, obj)
+    def __call__(self, *args, **kw):
+        obj = args[0]
+        try:
+            cache = obj.__cache
+        except AttributeError:
+            cache = obj.__cache = {}
+        key = (self.func, args[1:], frozenset(kw.items()))
+        try:
+            res = cache[key]
+        except KeyError:
+            res = cache[key] = self.func(*args, **kw)
+        return res
 
 #=============================================================================
 # Helper Classes
