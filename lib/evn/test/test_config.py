5ba9ef783d8b6c245114e941d872521ba64cd2dc
Trent Nelson <trent@trent.me>
2014-07-01 21:43:03 -0400

    Use evn.test scaffolding, update repo_name usage.

--- lib/evn/test/test_config.py
+++ lib/evn/test/test_config.py
@@ -15,6 +15,10 @@ from evn.config import (
     Config,
 )
 
+from evn.test import (
+    EnversionTest,
+)
+
 #=============================================================================
 # Helper Methods
 #=============================================================================
@@ -26,7 +30,7 @@ def suite():
 #=============================================================================
 # Test Classes
 #=============================================================================
-class TestConfig(unittest.TestCase):
+class TestConfig(EnversionTest, unittest.TestCase):
     def create_conf(self, repo_name=None):
         text = dedent(
             """
@@ -50,10 +54,9 @@ class TestConfig(unittest.TestCase):
             f.flush()
             f.close()
 
-        if repo_name is not None:
-            conf = Config(repo_name)
-        else:
-            conf = Config()
+        conf = Config()
+        if repo_name:
+            conf._repo_name = repo_name
 
         conf.load(tmpfile.name)
         return conf
@@ -63,23 +66,23 @@ class TestConfig(unittest.TestCase):
 
         self.assertEqual(c._g('verbose'), '1')
 
-        c.repo_name = 'foo'
+        c._repo_name = 'foo'
         self.assertEqual(c._g('verbose'), 'foo')
 
-        c.repo_name = 'bar'
+        c._repo_name = 'bar'
         self.assertEqual(c._g('verbose'), 'bar')
 
-        c.repo_name = 'main'
+        c._repo_name = 'main'
         self.assertEqual(c._g('verbose'), 'main')
 
     def test_repo_override2(self):
         c = self.create_conf('foo')
         self.assertEqual(c._g('verbose'), 'foo')
 
-        c.repo_name = 'bar'
+        c._repo_name = 'bar'
         self.assertEqual(c._g('verbose'), 'bar')
 
-        c.repo_name = 'main'
+        c._repo_name = 'main'
         self.assertEqual(c._g('verbose'), 'main')
 
         self.assertEqual(c._get('main', 'verbose'), '1')
@@ -89,8 +92,11 @@ class TestConfig(unittest.TestCase):
 
         self.assertEqual(c._g('fallback'), '1')
 
-if __name__ == '__main__':
+def main():
     runner = unittest.TextTestRunner()
     runner.run(suite())
+
+if __name__ == '__main__':
+    main()
 
 # vim:set ts=8 sw=4 sts=4 tw=78 et:
