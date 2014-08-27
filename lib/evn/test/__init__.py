ba4c8771cd6e634bd0a9750fb3cfadf85c3fb85c
Trent Nelson <trent@trent.me>
2014-08-27 19:14:36 -0400

    Improve testing infrastructure.
        - Handle multiple modules.
        - Respect -q/--quiet.

--- lib/evn/test/__init__.py
+++ lib/evn/test/__init__.py
@@ -2,12 +2,15 @@
 # Imports
 #===============================================================================
 import os
+import sys
 import unittest
 
 from os.path import (
     abspath,
     dirname,
 )
+
+from collections import defaultdict
 
 from abc import ABCMeta
 
@@ -35,19 +38,36 @@ def import_all(names):
     import importlib
     return [ importlib.import_module(name) for name in names ]
 
-def all_test_classes():
+def all_tests():
     import_all(test_module_names())
-    return EnversionTest.__subclasses__()
+    tests = defaultdict(list)
+    for test_class in EnversionTest.__subclasses__():
+        tests[test_class.__module__].append(test_class)
 
-def suite():
+    return tests
+
+def announce(stream, module_name, test_class):
+    stream.write('%s: %s\n' % (module_name, test_class))
+
+def suites(stream):
     loader = unittest.defaultTestLoader
-    return loader.loadTestsFromTestCase(*all_test_classes())
+    for (module_name, classes) in all_tests().items():
+        for test_class in classes:
+            announce(stream, module_name, test_class.__name__)
+            yield loader.loadTestsFromTestCase(test_class)
 
 #===============================================================================
 # Main
 #===============================================================================
 def main(quiet=None):
-    runner = unittest.TextTestRunner()
-    runner.run(suite())
+    if quiet:
+        stream = open('/dev/null', 'w')
+    else:
+        stream = sys.stdout
+
+    verbosity = int(not quiet)
+    runner = unittest.TextTestRunner(stream=stream, verbosity=verbosity)
+    for suite in suites(stream):
+        runner.run(suite)
 
 # vim:set ts=8 sw=4 sts=4 tw=78 et:
