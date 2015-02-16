d36734a3af05cf77a07ccfaff1d87c7a436ca189
Trent Nelson <trent@trent.me>
2015-02-16 11:25:41 -0500

    Implement support for blocked file extensions.  (Fixes #28.)

--- lib/evn/constants.py
+++ lib/evn/constants.py
@@ -136,6 +136,7 @@ class _Errors(Constant):
     TopLevelRepoComponentDirectoryRemoved = "top-level repository directories cannot be removed for component '%s'"
     TopLevelRepoComponentDirectoryReplaced = "top-level repository directories cannot be replaced for component '%s'"
     StandardLayoutTopLevelDirectoryCreatedInMultiComponentRepo = "standard layout top-level directories must be created within a component as this is a multi-component repository (i.e. try mkdir /foo/trunk instead of mkdir /trunk)"
+    BlockedFileExtension = "blocked file extension"
 
 e = _Errors()
 
