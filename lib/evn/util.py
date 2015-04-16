aa41b6cc8a4c8f26086bacfeba38e3e5385a0ca4
Trent Nelson <trent@trent.me>
2015-04-16 13:38:26 -0400

    Fix import_module by replacing it with stdlib implementation.

--- lib/evn/util.py
+++ lib/evn/util.py
@@ -467,13 +467,42 @@ class chdir(object):
     def __exit__(self, *exc_info):
         os.chdir(self.old_path)
 
-def import_module(modulename):
-    try:
-        # Python 2.7+ only.
-        import importlib
-        return importlib.import_module(modulename)
-    except ImportError:
-        return __import__(modulename)
+# _resolve_name and import_module taken from Python 2.7 (which was backported
+# from 3.x).  It's a simple module, definitely not worth the hassle of adding
+# a formal `importlib` dependency (i.e. from PyPI).  (We want this for Python
+# 2.6.)
+def _resolve_name(name, package, level):
+    """Return the absolute name of the module to be imported."""
+    if not hasattr(package, 'rindex'):
+        raise ValueError("'package' not set to a string")
+    dot = len(package)
+    for x in xrange(level, 1, -1):
+        try:
+            dot = package.rindex('.', 0, dot)
+        except ValueError:
+            raise ValueError("attempted relative import beyond top-level "
+                              "package")
+    return "%s.%s" % (package[:dot], name)
+
+def import_module(name, package=None):
+    """Import a module.
+
+    The 'package' argument is required when performing a relative import. It
+    specifies the package to use as the anchor point from which to resolve the
+    relative import to an absolute import.
+
+    """
+    if name.startswith('.'):
+        if not package:
+            raise TypeError("relative imports require the 'package' argument")
+        level = 0
+        for character in name:
+            if character != '.':
+                break
+            level += 1
+        name = _resolve_name(name[level:], package, level)
+    __import__(name)
+    return sys.modules[name]
 
 def load_class(classname, default_modulename=None):
     """
